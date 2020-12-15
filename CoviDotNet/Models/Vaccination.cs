bc07a1af33a198565405fed87f376dde30d12e64
Thibault Doucet <thibault.doucet44@gmail.com>
2020-12-15 16:34:37 +0100

    Ajoutez des fichiers projet.

---
+++ CoviDotNet/Models/Vaccination.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoviDotNet.Models
+{
+    public class Vaccination
+    {
+
+        [Key]
+        public int VaccinationId { get; set; }
+
+        public Person Person { get; set; }
+
+        public Vaccine Vaccine { get; set; }
+
+        public DateTime VaccinationDate { get; set; }
+
+        public string Lot { get; set; }
+
+        public DateTime ReminderDate { get; set; }
+
+    }
+}
---
+++ CoviDotNet/Models/Vaccine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoviDotNet.Models
+{
+    public class Vaccine
+    {
+
+        [Key]
+        public int VaccineId { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Disease { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public int ValidityPeriod { get; set; }
+
+    }
+}
